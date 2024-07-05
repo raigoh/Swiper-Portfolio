c3c54e7e5d83613d50ffaf6dc866e653fe36b783
raigoh <vikationu@gmail.com>
2024-07-05 10:44:35 +0300

    mob cube test

--- static/javascript/swiperModule.js
+++ static/javascript/swiperModule.js
@@ -1,28 +1,46 @@
-const swiper = new Swiper(".mySwiper", {
-  effect: "cube",
-  allowTouchMove: false,
-  grabCursor: false,
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  },
-  mousewheel: true,
-});
+function initSwiper() {
+  const isMobile = window.innerWidth <= 767;
 
-// Event listener for slide change in the main swiper
-swiper.on("slideChange", function () {
-  // Update active link in the navigation based on active index
-  const links = document.querySelectorAll(".Links li");
-  links.forEach((link) => link.classList.remove("activeLink"));
-  links[swiper.activeIndex].classList.add("activeLink");
-});
+  const swiperConfig = {
+    effect: "cube",
+    allowTouchMove: false,
+    grabCursor: false,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: isMobile ? 20 : 40,
+      shadowScale: isMobile ? 0.94 : 0.6,
+    },
+    mousewheel: true,
+  };
 
-// Function to navigate to a specific slide index
-function Navigate(indx) {
-  const links = document.querySelectorAll(".Links li");
-  links.forEach((link) => link.classList.remove("activeLink"));
-  links[indx].classList.add("activeLink");
-  swiper.slideTo(indx, 800, true);
+  // Initialize Swiper
+  const swiper = new Swiper(".mySwiper", swiperConfig);
+
+  // Event listener for slide change in the main swiper
+  swiper.on("slideChange", function () {
+    // Update active link in the navigation based on active index
+    const links = document.querySelectorAll(".Links li");
+    links.forEach((link) => link.classList.remove("activeLink"));
+    links[swiper.activeIndex].classList.add("activeLink");
+  });
+
+  // Function to navigate to a specific slide index
+  window.Navigate = function (indx) {
+    const links = document.querySelectorAll(".Links li");
+    links.forEach((link) => link.classList.remove("activeLink"));
+    links[indx].classList.add("activeLink");
+    swiper.slideTo(indx, 800, true);
+  };
+
+  return swiper;
 }
+
+// Initialize Swiper on page load
+let swiper = initSwiper();
+
+// Reinitialize Swiper on window resize
+window.addEventListener("resize", function () {
+  swiper.destroy(true, true); // Destroy existing instance
+  swiper = initSwiper(); // Reinitialize Swiper
+});
